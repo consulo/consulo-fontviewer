65548a37bcc33547ec0972724e4eb576cd8cf83a
Valery Semenchuk <vistall.valeriy@gmail.com>
2023-04-20 20:37:06 +0400

    this package not needed

--- api/src/main/java/module-info.java
+++ api/src/main/java/module-info.java
@@ -7,6 +7,5 @@ module consulo.fontviewer.api
 	requires transitive consulo.ide.api;
 
 	exports consulo.fontviewer;
-	exports consulo.fontviewer.icon;
 	exports consulo.fontviewer.internal to consulo.fontviewer.desktop.awt, consulo.fontviewer;
 }
